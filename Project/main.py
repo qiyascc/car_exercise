e2cf0a2f7b08c03ef508899b59cfbbb9be8d418f
Qiyas <107604130+qiyascc@users.noreply.github.com>
2024-04-14 06:38:11 +0400

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -2,11 +2,9 @@ import utils.data_loader as data_loader
 import models.classifier as classifier
 
 def main():
-    df = data_loader.load_data('./data/BMW_Vehicles_Data.csv')
+    df = data_loader.load_data('./')  
     X, y = data_loader.prepare_features(df)
-    
     classifier.train_evaluate(X, y)
 
 if __name__ == '__main__':
     main()
-  
